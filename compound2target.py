72d1ae383c428177f920d8fcb96894c835bc0652
Tran <44046910+thntran@users.noreply.github.com>
2020-10-23 11:32:33 +0800

    ChEMBL webresource client tutorial
    Having a list of molecules ChEMBL IDs in a CSV file, produce another CSV file that maps every compound ID into a list of Uniprot accession numbers and save the mapping into output CSV file.

---
+++ compound2target.py
@@ -0,0 +1,45 @@
+from numba import jit, cuda
+import csv
+from chembl_webresource_client.new_client import new_client
+
+
+# This will be our resulting structure mapping compound ChEMBL IDs into target uniprot IDs
+compounds2targets = dict()
+
+# First, let's just parse the csv file to extract compounds ChEMBL IDs:
+with open('D:/thnt/compounds_list.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        compounds2targets[row[0]] = set()
+
+# OK, we have our source IDs, let's process them in chunks:
+chunk_size = 50
+keys = list(compounds2targets.keys()) # for Python 3 we need to convert keys() to list
+
+for i in range(0, len(keys), chunk_size):
+    # we jump from compounds to targets through activities:
+    activities = new_client.activity.filter(molecule_chembl_id__in=keys[i:i + chunk_size]).only(['molecule_chembl_id', 'target_chembl_id'])
+    # extracting target ChEMBL IDs from activities:
+    for act in activities:
+        compounds2targets[act['molecule_chembl_id']].add(act['target_chembl_id'])
+
+# OK, now our dictionary maps from compound ChEMBL IDs into target ChEMBL IDs
+# We would like to replace target ChEMBL IDs with uniprot IDs
+
+for key, val in compounds2targets.items():
+    # We don't know how many targets are assigned to a given compound so again it's
+    # better to process targets in chunks:
+    lval = list(val)
+    uniprots = set()
+    for i in range(0, len(val), chunk_size):
+        targets = new_client.target.filter(target_chembl_id__in=lval[i:i + chunk_size]).only(['target_components'])
+        uniprots |= {comp['accession'] for t in targets for comp in t['target_components']}
+
+    compounds2targets[key] = uniprots
+
+# Finally write it to the output csv file
+with open('D:/thnt/compounds_2_targets.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    for key, val in compounds2targets.items():
+        writer.writerow([key] + list(val))
+
